57747e76638d8c28cd68fc2d1888e2f4a30a5129
Mayura21 <mayurasm2000@gmail.com>
2025-06-28 17:56:34 +0530

    Ml flow complete

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -15,6 +15,7 @@ from sklearn.metrics import r2_score
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import AdaBoostClassifier, GradientBoostingClassifier, RandomForestClassifier
+import mlflow
 
 
 class ModelTrainer:
@@ -24,6 +25,17 @@ class ModelTrainer:
             self.data_transformation_artifact = data_transformation_artifact
         except Exception as e:
             raise NetworkSecurityException(e, sys) 
+        
+    def track_mlflow(self, best_model, classification_metric):
+        with mlflow.start_run():
+            f1_score = classification_metric.f1_score
+            precision_score = classification_metric.precision_score
+            recall_score = classification_metric.recall_score
+
+            mlflow.log_metric("f1_score", f1_score)
+            mlflow.log_metric('precision_score', precision_score)
+            mlflow.log_metric("recall_score", recall_score)
+            mlflow.sklearn.log_model(best_model, "model")
         
     def train_model(self, X_train, y_train, x_test, y_test):
         models = {
@@ -72,10 +84,13 @@ class ModelTrainer:
 
         classification_train_metric = get_classification_score(y_true=y_train, y_pred=y_train_pred)
 
-        # Track mlflow
+        # Track experiments with mlflow
+        self.track_mlflow(best_model, classification_train_metric)
 
         y_test_pred = best_model.predict(x_test)
         classification_test_metric = get_classification_score(y_test, y_test_pred)
+
+        self.track_mlflow(best_model, classification_test_metric)
 
         preprocessor = load_object(file_path=self.data_transformation_artifact.transformed_object_file_path)
 
