01c6ceed0623f8d3828e2de4e3ecd0e93348bd67
Mayura21 <mayurasm2000@gmail.com>
2025-06-29 10:01:16 +0530

    Batch prediction

--- app.py
+++ app.py
@@ -5,9 +5,11 @@ from networksecurity.logging.logger import logging
 from networksecurity.pipeline.training_pipeline import TrainingPipeline
 from networksecurity.utils.main_utils.utils import load_object
 from networksecurity.constant.training_pipeline import DATA_INGESTION_COLLECTION_NAME, DATA_INGESTION_DATABSE_NAME
+from networksecurity.utils.ml_utils.model.estimator import NetworkModel
 
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, File, UploadFile, Request
+from fastapi.templating import Jinja2Templates
 from uvicorn import run as app_run
 from fastapi.responses import Response
 from starlette.responses import RedirectResponse
@@ -36,6 +38,7 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
+templates = Jinja2Templates(directory="./templates")
 
 @app.get("/", tags=["authentication"])
 async def index():
@@ -48,6 +51,24 @@ async def train_route():
         train_pipeline = TrainingPipeline()
         train_pipeline.run_pipeline()
         return Response("Training is successful.")
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
+    
+
+@app.post("/predict")
+async def predict_route(request: Request, file: UploadFile=File(...)): 
+    try:
+        df = pd.read_csv(file.file)
+        preprocessor = load_object('final_model/preprocessor.pkl')
+        final_model = load_object('final_model/model.pkl')
+        network_model = NetworkModel(preprocessor=preprocessor, model=final_model)
+        print(df.iloc[0])
+        y_pred = network_model.predict(df)
+        print(y_pred)
+        df['predicted_column'] = y_pred
+        df.to_csv('prediction_output/output.csv')
+        table_html = df.to_html(classes='table table-striped')
+        return templates.TemplateResponse("table.html", {'request': request, "table": table_html})
     except Exception as e:
         raise NetworkSecurityException(e, sys)
     
